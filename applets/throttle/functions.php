be2f8a39e24d13e11228a839ff1f02bdb0a3adb6
M Yakub Mizan <ctgcoder@gmail.com>
2015-12-12 13:51:01 +0600

    updated twiml

--- applets/throttle/functions.php
+++ applets/throttle/functions.php
@@ -7,7 +7,7 @@ function limit_exceeded($duration, $enabled, $instance_id, $limit){
 		return false; //throttling disabled
 
 	$number = normalize_phone_to_E164($_REQUEST['From']);
-	$data = get_data($number,$instance_id);
+	$data = PluginData::get($instance_id . "__" . $number);
 	$duration = intval($duration) * 60; //convert it into seconds
 	
 	if (!$data){
@@ -15,25 +15,22 @@ function limit_exceeded($duration, $enabled, $instance_id, $limit){
 			'limit' => 1,
 			'added' => time()
 			));
+		return false;
 	}
 
-	if ($data['limit'] >= $limit){
-		update_data($number,$instance_id);
+	//clean expired time
+	if (!empty($data['added']) && (time() - $data['added']) > $duration ){
+		PluginData::set($instance_id . "__" . $number, array(
+			'limit' => 1,
+			'added' => time()
+			));
+		$data = PluginData::get($instance_id . "__" . $number);
+	}
+
+	if ($data['limit'] >= $limit && (time() - $data['added']) <= $duration){
+		PluginData::set($instance_id . "__" . $number, array(
+			'limit' => $data['limit'] + 1
+			));
 		return true;
 	}
-}
-
-
-function update_expired_limit($duration, $number, $instance_id){
-	return true;
-}
-
-function get_data($number,$instance_id){
-	//stub
-	update_expired_limit();
-	return PluginData::get($instance_id . "__" . $number);
-}
-
-function update_data($number,$instance_id){
-	//stub
 }
