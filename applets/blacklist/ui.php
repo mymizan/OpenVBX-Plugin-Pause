7667c2d121d93dccba437e5342a2908e597e4106
M Yakub Mizan <ctgcoder@gmail.com>
2015-12-18 16:45:13 +0600

    updated UI

--- applets/blacklist/ui.php
+++ applets/blacklist/ui.php
@@ -1,11 +1,7 @@
 <div class="vbx-applet">
 	<h3>Blacklist</h3>
-	<div class="vbx-full-pane">
-		<fieldset class="vbx-input-container">
-			<p>Numbers to block - one number per line</p>
-			<textarea name="blacklist" class='large'><?php echo AppletInstance::getValue('blacklist'); ?></textarea>
-		</fieldset>
-	</div><!-- .vbx-split-pane -->
+	<p>Numbers to block - one number per line</p>
+	<textarea name="blacklist" class='large'><?php echo AppletInstance::getValue('blacklist'); ?></textarea>
 	<h2>Next</h2>
 	<div class="vbx-full-pane">
 		<?php echo AppletUI::DropZone('next'); ?>
